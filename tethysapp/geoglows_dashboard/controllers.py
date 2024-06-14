4392f0e671176e0366cbf5c6de6ce4ad35e211a0
Sonia-96 <sypku1996@gmail.com>
2024-06-14 10:40:40 -0600

    bugfix: show date of forecast plot

--- tethysapp/geoglows_dashboard/controllers.py
+++ tethysapp/geoglows_dashboard/controllers.py
@@ -135,13 +135,13 @@ def get_forecast_plot(request):
     reach_id = int(request.GET['reach_id'])
     forecast_file_path = os.path.join(cache_dir_path, f'forecast-{reach_id}.csv')
     if os.path.exists(forecast_file_path):
-        df_forecast = pd.read_csv(forecast_file_path, parse_dates=['time'], index_col=[0])
+        df_forecast = pd.read_csv(forecast_file_path, parse_dates=['datetime'], index_col=[0])
     else:
         url = f'https://geoglows.ecmwf.int/api/v2/forecast/{reach_id}'
         response = requests.get(url)
         if response.status_code != 200:
             raise RuntimeError(f'Failed to fetch data for the river {reach_id}: ' + response.text)
-        df_forecast = pd.read_csv(io.StringIO(response.text))
+        df_forecast = pd.read_csv(io.StringIO(response.text), index_col=[0])
         df_forecast.to_csv(forecast_file_path)
 
     plot = geoglows.plots.forecast(df=df_forecast)
